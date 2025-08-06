81889c0084cb796cbc99aa2c8ec47851f9cff33a
abdikhani30 <mahamudabdikhani@gmail.com>
2025-08-06 15:46:23 +0100

    Update App.tsx

--- Drivelink-2/client/src/App.tsx
+++ Drivelink-2/client/src/App.tsx
@@ -2,16 +2,16 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import MobileLayout from "@/components/layout/mobile-layout";
-import { VehicleProvider } from "@/contexts/vehicle-context";
-import Home from "@/pages/home";
-import Parking from "@/pages/parking";
-import Services from "@/pages/services";
-import Fines from "@/pages/fines";
-import Felix from "@/pages/felix";
-import More from "@/pages/more";
-import NotFound from "@/pages/not-found";
+import { TooltipProvider } from "./components/ui/tooltip";
+import MobileLayout from "./components/layout/mobile-layout";
+import { VehicleProvider } from "./contexts/vehicle-context";
+import Home from "./pages/home";
+import Parking from "./pages/parking";
+import Services from "./pages/services";
+import Fines from "./pages/fines";
+import Felix from "./pages/felix";
+import More from "./pages/more";
+import NotFound from "./pages/not-found";
 
 function Router() {
   return (
