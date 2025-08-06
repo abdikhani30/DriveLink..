dd4a07322735eb99abb15061fe9f65c9bf2ed43c
abdikhani30 <mahamudabdikhani@gmail.com>
2025-08-06 15:35:16 +0100

    Update App.tsx

--- Drivelink-2/client/src/App.tsx
+++ Drivelink-2/client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import MobileLayout from "@/components/layout/mobile-layout";
 import { VehicleProvider } from "@/contexts/vehicle-context";
